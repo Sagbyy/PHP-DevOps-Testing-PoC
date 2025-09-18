169e0c674d4a6f148cbbe0a1037c1cd4cd2777fb
Abd-Phenix <abdelrahman.mohamed@phenixsp.com>
2025-09-18 11:58:17 +0200

    Add mock for PaypalService in InvoiceTest to verify payment processing

--- tests/Unit/InvoiceTest.php
+++ tests/Unit/InvoiceTest.php
@@ -2,6 +2,7 @@
 
 use App\Invoice;
 use App\InvoiceLine;
+use App\PaypalService;
 use Mockery;
 
 $invoice_line = new InvoiceLine($article, 5);
@@ -9,6 +10,8 @@ $invoice_line = new InvoiceLine($article, 5);
 it("add invoice line", function() use($invoice_line) {
     $invoice = new Invoice();
     $invoice->add_invoice_line($invoice_line);
+    $paypal_service = Mockery::mock(PaypalService::class);
+    $paypal_service->shouldReceive('send_payment');
 
     expect(count($invoice->get_invoice_lines()))->toEqual(1);
 });
